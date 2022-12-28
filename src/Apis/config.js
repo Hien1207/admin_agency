7b4261a5f44bbe6136836be9dfd68e5424c83363
lythanhhai <lythanhhait@gmail.com>
2022-12-28 22:07:10 +0700

    Updates

--- src/Apis/config.js
+++
@@ -1,3 +0,0 @@
-const baseUrl = "http://35.240.208.147:6789/api/";
-
-export default baseUrl;
--- src/Constants/auth.js
+++
@@ -1,5 +0,0 @@
-const USER_ROLE = {
-  Agency: "Agency",
-  ADMIN: "Admin",
-};
-export default USER_ROLE;
